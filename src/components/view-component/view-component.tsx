4b1666c58469df0491f215ba4ed793a15da6750b
obergodmar <obergodmar@gmail.com>
2020-06-29 18:57:14 +0300

    Fix version

--- src/components/view-component/view-component.tsx
+++ src/components/view-component/view-component.tsx
@@ -39,7 +39,7 @@ export const ViewComponent = ({src}: Props) => {
                 <div className={`view-background ${isLoaded ? 'view-background--loaded' : ''}`}/>
                 <div className='view-author'>
                     <a href="https://github.com/obergodmar">obergodmar</a>
-                    <span>1.0.0</span>
+                    <span>1.1.0</span>
                 </div>
             </div>
     )
