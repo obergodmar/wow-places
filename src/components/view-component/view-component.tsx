9ef7d60c82ab76ac4a648ce8860b95f03d42f461
obergodmar <obergodmar@gmail.com>
2020-09-12 15:51:26 +0300

    Add useViewScroll hook for view component

--- src/components/view-component/view-component.tsx
+++ src/components/view-component/view-component.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { FocusEvent, MouseEvent, TouchEvent, useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import { Background } from '../../assets';
-import { ANIMATION_DURATION, DEFAULT_HEIGHT, DEFAULT_WIDTH } from '../../utils';
+import { ANIMATION_DURATION } from '../../utils';
+import { useViewScroll } from '../../hooks';
 
 import './view-component.scss';
 
@@ -11,44 +12,13 @@ interface Props {
     src: string;
 }
 
-interface Position {
-    x: number;
-    y: number;
-}
-
-const initialPosition = {
-    x: 0,
-    y: 0,
-};
-
 export const ViewComponent: React.FC<Props> = ({ src }: Props) => {
     const [imageSrc, setImageSrc] = useState(Background);
     const [isLoaded, setLoaded] = useState(false);
-    const [isDrag, setDrag] = useState(false);
-    const [trackPosition, setTrackPosition] = useState(initialPosition);
-    const [position, setPosition] = useState(initialPosition);
-    const [lastPosition, setLastPosition] = useState(initialPosition);
-    const [isBigScreen, setBigScreen] = useState(false);
 
-    const handleResize = useCallback(() => {
-        const { innerWidth, innerHeight } = window;
-
-        let width = 0;
-        let height = 0;
-        setBigScreen(false);
-
-        if (innerHeight < DEFAULT_HEIGHT && innerWidth < DEFAULT_WIDTH) {
-            width = (innerWidth - DEFAULT_WIDTH) / 2;
-            height = (innerHeight - DEFAULT_HEIGHT) / 2;
-        } else {
-            setBigScreen(true);
-        }
-        setPosition({ x: width, y: height });
-        setLastPosition({ x: width, y: height });
-    }, []);
+    const { style, props } = useViewScroll();
 
     useEffect(() => {
-        handleResize();
         setLoaded(false);
         const timer = setTimeout(() => {
             const image = new Image();
@@ -61,104 +31,16 @@ export const ViewComponent: React.FC<Props> = ({ src }: Props) => {
         return () => {
             clearTimeout(timer);
         };
-    }, [src, handleResize]);
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [handleResize]);
-
-    const limiter = (value: Position, width: number, height: number) => {
-        const { x: xValue, y: yValue } = value;
-
-        let x = lastPosition.x - trackPosition.x + xValue;
-        let y = lastPosition.y - trackPosition.y + yValue;
-
-        if (x > 0) {
-            x = 0;
-        } else if (x < -width) {
-            x = -width;
-        }
-
-        if (y > 0) {
-            y = 0;
-        } else if (y < -height) {
-            y = -height;
-        }
-
-        return { x, y };
-    };
-
-    const handleTouchMove = (e: TouchEvent) => {
-        const { touches } = e;
-        if (isBigScreen) {
-            return;
-        }
-        const { innerWidth, innerHeight } = window;
-        const width = DEFAULT_WIDTH - innerWidth;
-        const height = DEFAULT_HEIGHT - innerHeight;
-        const { clientX: x, clientY: y } = touches[0];
-
-        const diff = limiter({ x, y }, width, height);
-
-        setPosition(diff);
-    };
-
-    const handleTouchstart = (e: TouchEvent) => {
-        const { touches } = e;
-        e.nativeEvent.stopImmediatePropagation();
-        const { clientX: x, clientY: y } = touches[0];
-        setTrackPosition({ x, y });
-        setDrag(true);
-    };
-
-    const handleMouseDown = (e: MouseEvent) => {
-        const { clientX, clientY } = e;
-        e.nativeEvent.stopImmediatePropagation();
-        setTrackPosition({ x: clientX, y: clientY });
-        setDrag(true);
-    };
-
-    const handleFree = (e: MouseEvent | FocusEvent | TouchEvent) => {
-        e.nativeEvent.stopImmediatePropagation();
-        setDrag(false);
-        setLastPosition(position);
-    };
-
-    const handleMouseMove = (e: MouseEvent) => {
-        if (!isDrag) {
-            return;
-        }
-        if (isBigScreen) {
-            return;
-        }
-        const { innerWidth, innerHeight } = window;
-        const width = DEFAULT_WIDTH - innerWidth;
-        const height = DEFAULT_HEIGHT - innerHeight;
-
-        const { clientX: x, clientY: y } = e;
-        const diff = limiter({ x, y }, width, height);
-        setPosition(diff);
-    };
+    }, [src]);
 
     return (
         <div
+            {...props}
             className="view"
             style={{
                 backgroundImage: `url(${imageSrc})`,
-                backgroundPosition: `${position.x}px ${position.y}px`,
-                backgroundSize: `${isBigScreen ? 'cover' : 'auto'}`,
+                ...style,
             }}
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleFree}
-            onTouchMove={handleTouchMove}
-            onTouchStart={handleTouchstart}
-            onTouchEnd={handleFree}
-            onMouseLeave={handleFree}
-            onBlur={handleFree}
         >
             <div
                 className={cn('view-background', {
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useSettings } from './use-settings';
 export { useUiSound } from './use-ui-sound';
 export { usePanelScroll } from './use-panel-scroll';
 export { usePlaceView } from './use-place-view';
+export { useViewScroll } from './use-view-scroll';
